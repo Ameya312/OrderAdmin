6530c2f49b1e382f19c9b70b915c3dd67021b03f
Akash Barne <akash.barne@incedoinc.com>
2019-08-16 14:46:32 +0530

    Login Hardcode

--- src/App.js
+++ src/App.js
@@ -3,20 +3,66 @@ import ReactDOM from 'react-dom';
 import My_order from'./Scripts/my_order';
 import './App.css'
 import incedo from './download.png'
+import AdminHome from './OnlinePosts/AdminHome'
 
-function App() {
+class App extends  React.Component {
+  constructor(props){
+    super(props);
+    this.state = { //state is by default an object
+      Username:"blank",
+      PWD:"blank",
+   }
+   this.handleUsername = this.handleUsername.bind(this);
+   this.handlePass = this.handlePass.bind(this);
+
+  }
+  checkUser(){
+  if(this.state.Username=="admin" && this.state.PWD=="1234")
+{
+  ReactDOM.render(<AdminHome/>, document.getElementById('root'))
+}
+else if(this.state.Username=="user" && this.state.PWD=="1234")
+{
+  ReactDOM.render(<My_order/>, document.getElementById('root'))
+}
+else{
+  alert("invalid credential");
+}
+
+}
+
+handleUsername(e){
+  this.setState({Username:e.target.value});
+}
+handlePass(e){
+  this.setState({PWD:e.target.value});
+}
+  
+  render (){
+
   return (
     <body>
       <div className="App">
         <header className="App-header">
           <center>
-            <img src={incedo} class="cent" alt="Incedo Logo" width="50%"></img><br/>
-            <button onClick = {()=>ReactDOM.render(<My_order/>, document.getElementById('root'))} class="appB">Place My order</button>
+              <img src={incedo} alt="Incedo Logo" width="50%"/>
+              <table>
+              <tr>
+                <td><label>userId</label></td><td><input type="text" placeholder="enter userId" name="formUserName"  onChange={this.handleUsername}></input></td>
+              </tr>
+              <tr>
+                <td><label>password</label></td><td><input type="password" placeholder="enter password" name="PWD" onChange={this.handlePass}></input></td>
+              </tr>
+              <tr>
+                <td><button onClick = {()=>this.checkUser()} class="appB">login</button></td>
+              </tr>
+            </table>
           </center>      
         </header>
       </div>
     </body>
-  );
+  )
+  }
 }
 
 export default App;
