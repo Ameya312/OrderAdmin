eda680422d80bdfaac73edc2eccc895205aa4669
Akash Barne <akash.barne@incedoinc.com>
2019-08-19 15:21:42 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import My_order from'./Scripts/my_order';
 import './App.css'
 import incedo from './download.png'
 import AdminHome from './OnlinePosts/AdminHome'
+import Register from './Scripts/Register';
 
 class App extends  React.Component {
   constructor(props){
@@ -11,41 +12,53 @@ class App extends  React.Component {
     this.state = { //state is by default an object
       Username:"blank",
       PWD:"blank",
-      LoginStatus: false,
+      LoginDetail: false,
    }
    this.handleUsername = this.handleUsername.bind(this);
    this.handlePass = this.handlePass.bind(this);
 
   }
-  checkUser(){
-  if(this.state.Username=="admin" && this.state.PWD=="1234")
-{
-  ReactDOM.render(<AdminHome/>, document.getElementById('root'))
-}
-else if(this.state.Username=="user" && this.state.PWD=="1234")
-{
-  ReactDOM.render(<My_order/>, document.getElementById('root'))
-}
-else{
-  alert("invalid credential");
-}
 
-}
 fetchCredintials(){
-  fetch("http://localhost:8080/login?userName="+this.state.Username+"&userName="+this.state.PWD+"", {
-
-      method:"GET"
+  fetch('http://localhost:8080/login', {
+      method:'POST',
+      headers: {     
+        "Content-type": "application/json; charset=UTF-8"
+      },
+      body: JSON.stringify({
+        username:this.state.Username,
+        password:this.state.PWD,
+      }),
   }).then(response => {
 console.log(response)
           return response.text()
       }).then(json => {
           this.setState({ 
               LoginDetail:json
+          }, () => {
+            this.changePage();
           });
       });
 }
 
+changePage = ()=>{
+  console.log(this.state.LoginDetail)
+if(this.state.LoginDetail=="USER")
+{
+ReactDOM.render(<My_order/>, document.getElementById('root'))
+}
+else if(this.state.LoginDetail=="ADMIN")
+{
+ReactDOM.render(<AdminHome/>, document.getElementById('root'))
+}
+if(this.state.LoginDetail=="NOT REGISTERED")
+{
+alert("Incorrect Credintials");
+}
+  
 
+
+}
 
 handleUsername(e){
   this.setState({Username:e.target.value});
@@ -54,6 +67,9 @@ handlePass(e){
   this.setState({PWD:e.target.value});
 }
   
+signUp(){
+  ReactDOM.render(<Register/>, document.getElementById('root'))
+}
   render (){
 
   return (
@@ -70,7 +86,8 @@ handlePass(e){
                 <td><label>password</label></td><td><input type="password" placeholder="enter password" name="PWD" onChange={this.handlePass}></input></td>
               </tr>
               <tr>
-                <td><button onClick = {()=>this.checkUser()} class="appB">login</button></td>
+                <td><button onClick = {()=>this.fetchCredintials()} class="appB">login</button></td>
+                <td><button class="appC" onClick={()=>this.signUp()} >Sign Up</button></td>
               </tr>
             </table>
           </center>      
---
+++ src/Scripts/Register.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import '../Styles/orderSummary.css';
+import incedo from '../download.png'
+import App from '../App';
+import ReactDOM from 'react-dom';
+
+
+class Register extends React.Component {
+
+  constructor(props){
+      super(props);
+      this.state = { //state is by default an object
+        Registration_status:"NOT registered",
+        EmployeeId : "",
+        EmployeeName:"",
+        EmployeeContact : "",
+        userName : "",
+        password:"",
+        role:"USER"
+     }
+     this.handleEmployeeName = this.handleEmployeeName.bind(this);
+     this.handleEmployeeId = this.handleEmployeeId.bind(this);
+     this.handleEmployeeContact = this.handleEmployeeContact.bind(this);
+     this.handlepassword = this.handlepassword.bind(this);
+
+  }
+  handleEmployeeName(e){
+    this.setState({EmployeeName:e.target.value});
+  }
+  handleEmployeeId(e){
+    this.setState({EmployeeId:e.target.value,
+                  userName:e.target.value
+                  });
+  }
+  handleEmployeeContact(e){
+    this.setState({EmployeeContact:e.target.value});
+  }
+  handlepassword(e){
+    this.setState({password:e.target.value});
+  }
+
+  registerUser(){
+    debugger;
+    fetch('http://localhost:8080/register', {
+      method:'POST',
+      headers: {     
+        "Content-type": "application/json; charset=UTF-8"
+      },
+      body: JSON.stringify({
+        login_details :{
+          username:this.state.userName,
+          password:this.state.password,
+          role:this.state.role
+        },
+        
+        user_details :{
+          emp_id: this.state.EmployeeId,
+          name:this.state.EmployeeName,
+          phone_no: this.state.EmployeeContact
+        }
+      }),
+  }).then(response => {
+console.log(response)
+          return response.text()
+      }).then(json => {
+          this.setState({ 
+              Registration_status:json
+          }, () => {
+            alert(this.state.Registration_status);
+            if(this.state.Registration_status=="Successfully Register" || this.state.Registration_status=="User Exist"){
+              ReactDOM.render(<App/>, document.getElementById('root'))
+            }else if(this.state.Registration_status=="Failed To register"){
+              ReactDOM.render(<Register/>, document.getElementById('root'))
+            }
+          });
+      });
+
+  }
+  render(){
+    return(
+          <div > 
+            <center>
+              <img src={incedo} alt="Incedo Logo" width="50%"/>
+              </center>
+                <table >
+                <tr><td><label>EMPLOYEE NAME</label></td><td><input type="text"  name="name"  onChange={this.handleEmployeeName}></input></td></tr>
+                <tr><td><label>EMPLOYEE ID</label></td><td><input type="text"  name="Id"  onChange={this.handleEmployeeId}></input></td></tr>
+                <tr><td><label>Contact No.</label></td><td><input type="text"  name="contact"  onChange={this.handleEmployeeContact}></input></td></tr>
+                <tr><td><label>Password</label></td><td><input type="password"  name="password"  onChange={this.handlepassword}></input></td></tr>
+                </table>
+                 <center><button onClick={()=>this.registerUser()} >Sign Up</button></center>
+          </div>   
+
+    )
+  }
+}
+
+export default Register;
